f91f26debbd3d566cda2a1f13c89eb9a0167fb3f
xsolagul <mark_kuuga@hotmail.com>
2021-02-03 12:28:37 +0700

    message

---
+++ 62130500036_groupwork_2/profile.js
@@ -0,0 +1,21 @@
+const app = {
+    data() {
+        return {
+            msg: 'Hello, Vue3',
+            name:"thanasit eksoragul" ,
+            job:"student",
+            one: "fds",
+            onevalue: "2",
+            two:"sdd",
+            twovalue:"4",
+            three:"aaa",
+            threevalue:"67",
+            box1:"chat1",
+            box2:"chat2",
+            image:"./images/preview.jpg"
+
+        }
+    }
+
+}
+moutedApp = Vue.createApp(app).mount('#app')
